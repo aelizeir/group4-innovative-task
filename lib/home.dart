e37c2294f4544d9101dd17f4cf01f99f01f3e631
aelizeir <116530431+aelizeir@users.noreply.github.com>
2022-10-26 16:12:38 +0800

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -184,7 +184,7 @@ class _HomeState extends State<Home> with SingleTickerProviderStateMixin {
 
   @override
   void initState() {
-    tabController = TabController(length: 5, vsync: this);
+    tabController = TabController(length: 6, vsync: this);
     super.initState();
 
   }

f54e2b251eb7e5e12eb6b0666b3d330576a462f1
Ali Gutierrez <116530431+aelizeir@users.noreply.github.com>
2022-10-31 15:40:37 +0800

    Update page_two.dart
    Profile pic, cover photo, and some minimal changes

--- lib/pages/page_two.dart
+++ lib/pages/page_two.dart
@@ -5,6 +5,8 @@ import 'package:innovative_task/models/family.dart';
 class PageTwo extends StatelessWidget {
   final Family information;
   final List<String> titles = <String>['Name', 'Relationship', 'Occupation', 'Birthday', 'Age'];
+  final double coverHeight = 280;
+  final double profileHeight = 144;
 
     PageTwo({
     required this.information,
@@ -12,70 +14,80 @@ class PageTwo extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final top = coverHeight - profileHeight / 2;
+    final bottom = profileHeight / 2;
     return Scaffold(
+        backgroundColor: const Color(0xFFFAF9F6),
         appBar: AppBar(
           backgroundColor: const Color(0xFFFF9800),
-          title: Text(information.name ?? ' '),
+          title: Text(information.name ?? ' ', style: const TextStyle(color: Colors.white)),
         ),
         body: ListView(
-            padding: const EdgeInsets.only(left:2),
-            shrinkWrap: true,
+            padding: EdgeInsets.zero,
             children: <Widget>[
-              Container(
+              Stack(
+                clipBehavior: Clip.none,
                 alignment: Alignment.center,
-                padding: const EdgeInsets.all(32),
-                decoration: const BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('assets/images/bg.jpg'),
-                    fit: BoxFit.cover,
-                      colorFilter: ColorFilter.mode(Colors.black, BlendMode.dstATop)
-                  ),
-                ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                    Padding(
-                      padding: const EdgeInsets.all(65.0),
+                  Container(
+                    margin: EdgeInsets.only(bottom: bottom),
+                    color: Colors.orangeAccent,
+                    child: Image.asset('assets/images/group4.jpg',
+                        width: double.infinity,
+                        height: coverHeight,
+                        fit: BoxFit.cover),
+                  ),
+                  Positioned(
+                    top: top,
+                    child: CircleAvatar(
+                      radius: profileHeight,
+                      backgroundColor: const Color(0xFFFAF9F6),
                       child: CircleAvatar(
-                      radius: 120,
-                      backgroundImage: AssetImage(information.image.toString()),
+                        radius: 135,
+                        backgroundImage: AssetImage(information.image.toString()),
                       ),
-                    ),
-                ]
+                    )
+                  ),
+                ],
               ),
-
-              ListTile(
-                shape: RoundedRectangleBorder(
+              const SizedBox(
+                height: 200,
+              ),
+              Center(
+                child: ListTile(
+                  leading: const Icon(Icons.person, color: Colors.white70),
+                  minVerticalPadding: 2,
+                  shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(10),
-                ),
-                tileColor: const Color(0xFFFF9800),
-                textColor: Colors.black,
-                title: Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Expanded(
-                      flex: 4,
-                    child: Text('Name'),
-                    ),
-                    Expanded(
-                    flex: 9,
-                    child: Text(': ${information.name}'),
-                    ),
-                  ],
+                  ),
+                  tileColor: const Color(0xFFFF9800),
+                  textColor: Colors.black,
+                  title: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Expanded(
+                        flex: 4,
+                        child: Text('Name'),
+                      ),
+                      Expanded(
+                        flex: 9,
+                        child: Text(': ${information.name}'),
+                      ),
+                    ],
+                  ),
                 ),
               ),
               const SizedBox(
                 height: 6,
               ),
               ListTile(
+                leading: const Icon(Icons.people_rounded, color: Colors.white70),
                 minVerticalPadding: 2,
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(10),
                 ),
-                tileColor: const Color(0xFFFF9800),
+                tileColor: const Color(0xFFFFCBA4),
                 textColor: Colors.black,
                 title: Row(
                   mainAxisAlignment: MainAxisAlignment.start,
@@ -96,6 +108,7 @@ class PageTwo extends StatelessWidget {
                 height: 6,
               ),
               ListTile(
+                leading: const Icon(Icons.work, color: Colors.white70),
                 minVerticalPadding: 2,
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(10),
@@ -121,11 +134,12 @@ class PageTwo extends StatelessWidget {
                 height: 6,
               ),
               ListTile(
+                leading: const Icon(Icons.cake, color: Colors.white70),
                 minVerticalPadding: 2,
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(10),
                 ),
-                tileColor: const Color(0xFFFF9800),
+                tileColor: const Color(0xFFFFCBA4),
                 textColor: Colors.black,
                 title: Row(
                   mainAxisAlignment: MainAxisAlignment.start,
@@ -146,6 +160,7 @@ class PageTwo extends StatelessWidget {
                 height: 6,
               ),
               ListTile(
+                leading: const Icon(Icons.yard, color: Colors.white70),
                 minVerticalPadding: 2,
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(10),
@@ -167,8 +182,11 @@ class PageTwo extends StatelessWidget {
                   ],
                 ),
               ),
-            ],
+              const SizedBox(
+                height: 100,
+              ),
+           ],
         ),
-    );
-  }
+      );
+   }
 }
